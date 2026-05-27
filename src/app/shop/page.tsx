7f2f32fed61e18ea3e998699dd68a88dcd4b654b
Augustine Mayaki <2509984@leedstrinity.ac.uk>
2026-05-27 16:14:51 +0100

    implement shop page

--- src/app/shop/page.tsx
+++ src/app/shop/page.tsx
@@ -1,9 +1,24 @@
+import { ShopProducts } from "@/components/shopProducts";
 import { AppLayout } from "../appLayout";
 
 const Shop = () => {
   return (
     <AppLayout>
-      <div>Shop</div>
+      <section className=" relative">
+        <div className="text-center 768:text-start">
+          <p className="text-sm uppercase tracking-[0.3em] text-foreground/60">
+            Discover eyewear
+          </p>
+
+          <h1 className="text-5xl 768:text-7xl 1240:text-[80px] font-bold leading-none mt-4">
+            FIND YOUR
+            <br />
+            PERFECT FRAME
+          </h1>
+        </div>
+
+        <ShopProducts />
+      </section>
     </AppLayout>
   );
 };
